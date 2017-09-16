e801b7e81d0820b152102af370d67b5daf247f11
whoo <prettywhoo@163.com>
2017-09-16 18:52:31 +0800

    up

--- felink/js/global.js
+++ felink/js/global.js
@@ -10,6 +10,15 @@ $(document).ready(function() {
         return false;
     });
 
+    $(".accordion-menu li").click(function(){
+        $(".off-canvas").hide();
+        $(".js-off-canvas-overlay").css("visibility","hidden");
+    });
+
+    $(".footer").click(function(){
+        $('body').animate({'scrollTop':'0'},300);
+    });
+
     $(".link-item .item").mouseover(function() {
         $(this).addClass("animated bounce").siblings().removeClass("animated bounce");
     });
