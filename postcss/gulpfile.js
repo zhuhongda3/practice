21b2f1f1e91ddee495ae53bc3a7c199095f6f0ba
wangtaohua <wangtaohua@mallcoo.com>
2018-06-01 11:05:53 +0800

    add

--- postcss/gulpfile.js
+++ postcss/gulpfile.js
@@ -1,16 +1,43 @@
 var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
 
-gulp.task('css', function () {
-  var postcss = require('gulp-postcss');
-  var sourcemaps = require('gulp-sourcemaps');
+ /**
+ * outputStyle
+ * 嵌套(默认):nested
+ * 展开:expanded
+ * 紧凑:compact
+ * 压缩:compressed
+ */
 
+/**
+ * 模拟嵌套语法:precss
+ * 下一代CSS:postcss-preset-env
+ * 浏览器前缀:autoprefixer
+ */
+
+gulp.task('scss', function () {
   return gulp.src('src/**/*.scss')
     .pipe(sourcemaps.init())
+    .pipe(sass({ 
+      outputStyle: "compact"
+     }).on("error", sass.logError))
     .pipe(postcss([
-      require('precss'), 
       require('autoprefixer'),
-      require('cssnano'),
     ]))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('build/'));
 });
+
+// gulp.task('css', function () {
+//   return gulp.src('src/**/*.css')
+//     .pipe(sourcemaps.init())
+//     .pipe(postcss([
+//       require('precss'),
+//       // require('postcss-preset-env'),
+//       require('autoprefixer'),
+//     ]))
+//     .pipe(sourcemaps.write('.'))
+//     .pipe(gulp.dest('build/'));
+// });
