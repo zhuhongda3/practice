d4e935c165a0282c315a41bf13d7c689f2fe0d95
whoo <prettywhoo@163.com>
2018-06-22 00:26:39 +0800

    vue

--- gulp/note/src/js/test.js
+++ gulp/note/src/js/test.js
@@ -3,6 +3,89 @@ var vm = new Vue({
   data: {
     show: false,
     show1: false,
+    dataList: [],
+  },
+  mounted: function(){
+    this.dataList = [
+      {
+        year: 2018,
+        monthList: [
+          {
+            month: "2018-01",
+            articleList: [
+              {
+                title: '文章1',
+                url: '#'
+              },
+              {
+                title: '文章2',
+                url: '#'
+              },
+              {
+                title: '文章3',
+                url: '#'
+              },
+            ]
+          },
+          {
+            month: "2018-02",
+            articleList: [
+              {
+                title: '文章1',
+                url: '#'
+              },
+              {
+                title: '文章2',
+                url: '#'
+              },
+              {
+                title: '文章3',
+                url: '#'
+              },
+            ]
+          },
+        ]
+      },
+      {
+        year: 2017,
+        monthList: [
+          {
+            month: "2017-01",
+            articleList: [
+              {
+                title: '文章1',
+                url: '#'
+              },
+              {
+                title: '文章2',
+                url: '#'
+              },
+              {
+                title: '文章3',
+                url: '#'
+              },
+            ]
+          },
+          {
+            month: "2017-01",
+            articleList: [
+              {
+                title: '文章1',
+                url: '#'
+              },
+              {
+                title: '文章2',
+                url: '#'
+              },
+              {
+                title: '文章3',
+                url: '#'
+              },
+            ]
+          },
+        ]
+      },
+    ]
   },
   methods: {
     
