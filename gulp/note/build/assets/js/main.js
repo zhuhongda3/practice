1fa5bc2484d9fc31b34b92d5a7ce127ac9f4b214
whoo <prettywhoo@163.com>
2018-06-24 22:29:42 +0800

    add

--- gulp/note/build/assets/js/main.js
+++ gulp/note/build/assets/js/main.js
@@ -2,71 +2,65 @@
 
 var DelItem = Vue.extend({
   props: ['item1', 'item2', 'index2'],
-  template: '<li ref="currentItem" :index="index2">' + '<a :href="item2.url">' + '<span class="del-btn" @click.prevent="deleteItem(item1.children,index2)" ref="remove">删除</span>' + '<p' + '@touchstart="_touchStart"' + '@touchmove.prevent="_touchMove"' + '@touchend="_touchEnd"' + ':style="txtStyle">' + '{{item2.name}}' + '</p>' + '</a>' + '</li>',
+  template: '<li ref="currentItem" :index="index2">' + '<a :href="item2.url">' + '<span class="del-btn" @click.prevent="deleteItem(item1.children,index2)" ref="remove">删除</span>' + '<p' + '@touchstart="_touchStart(item2)"' + '@touchmove.prevent="_touchMove(item2)"' + '@touchend="_touchEnd(item2)"' + ':style="item2.style">' + '{{item2.name}}' + '</p>' + '</a>' + '</li>',
   data: function data() {
     return {
       startX: 0, //触摸位置
       moveX: 0, //滑动时的位置
-      disX: 0, //移动距离
-      txtStyle: ""
+      disX: 0 //移动距离
     };
   },
-  computed: {},
   methods: {
-    _touchStart: function _touchStart(ev) {
-      ev = ev || event;
+    computeStyle: function computeStyle(result) {
+      return 'transform: translateX(' + result + 'px)';
+    },
+    _touchStart: function _touchStart(current) {
+      var ev = ev || event;
       var w = this.$refs.remove.offsetWidth;
+      var _this = this;
       if (ev.touches.length == 1) {
-        var idx = this.$refs.currentItem.getAttribute('index');
-        //当前元素的父元素ul的子元素li的个数
-        var len = this.$refs.currentItem.parentNode.children.length;
-        var current = this.$refs.currentItem.parentNode.children[idx].getElementsByTagName('p')[0].getAttribute('style');
-        if (typeof current != 'undefined' && current != null && current.match(/\d+/)[0] == String(w)) {
-          ev.preventDefault();
-        }
-        for (var i = 0; i < len; i++) {
-          if (i == idx) continue;
-          var item = this.$refs.currentItem.parentNode.children[i];
-          var s = item.getElementsByTagName('p')[0].getAttribute('style');
-          if (typeof s != 'undefined' && s != null && s.match(/\d+/)[0] == String(w)) {
-            item.getElementsByTagName('p')[0].style.transform = 'translateX(0px)';
+        if (typeof current.style != 'undefined' && current.style != null && current.style.match(/\d+/)[0] == String(w)) ev.preventDefault();
+        this.item1.children.forEach(function (item) {
+          if (item == current) return false;
+          if (typeof item.style != 'undefined' && item.style != null && item.style.match(/\d+/)[0] == String(w)) {
+            _this.$set(item, 'style', _this.computeStyle('0'));
             ev.preventDefault();
           }
-        }
+        });
         this.startX = ev.touches[0].clientX;
       }
     },
-    _touchMove: function _touchMove(ev) {
-      ev = ev || event;
+    _touchMove: function _touchMove(current) {
+      var ev = ev || event;
       var w = this.$refs.remove.offsetWidth;
       if (ev.touches.length == 1) {
         this.moveX = ev.touches[0].clientX;
         this.disX = this.startX - this.moveX;
         if (this.disX < 0 || this.disX == 0) {
-          this.txtStyle = "transform: translateX(0px)";
+          this.$set(current, 'style', this.computeStyle('0'));
         } else {
-          this.txtStyle = "transform: translateX(-" + this.disX + "px)";
+          this.$set(current, 'style', this.computeStyle('-' + this.disX));
           if (this.disX >= w) {
-            this.txtStyle = "transform: translateX(-" + w + "px)";
+            this.$set(current, 'style', this.computeStyle('-' + w));
           }
         }
       }
     },
-    _touchEnd: function _touchEnd(ev) {
-      ev = ev || event;
+    _touchEnd: function _touchEnd(current) {
+      var ev = ev || event;
       var w = this.$refs.remove.offsetWidth;
       if (ev.changedTouches.length == 1) {
         var endX = ev.changedTouches[0].clientX;
         this.disX = this.startX - endX;
         if (this.disX < w / 2) {
-          this.txtStyle = "transform: translateX(0px)";
+          this.$set(current, 'style', this.computeStyle('0'));
         } else {
-          this.txtStyle = "transform: translateX(-" + w + "px)";
+          this.$set(current, 'style', this.computeStyle('-' + w));
         }
       }
     },
     deleteItem: function deleteItem(item, index) {
-      this.txtStyle = "transform: translateX(0px)";
+      this.computeStyle('0');
       item.splice(index, 1);
     }
   }
@@ -81,10 +75,8 @@ new Vue({
   },
   mounted: function mounted() {
     this.dataList = [{
-      show: false,
       name: 2018,
       children: [{
-        show: false,
         name: "2018-01",
         children: [{
           name: "文章1",
@@ -94,18 +86,16 @@ new Vue({
           url: "//baidu.com"
         }]
       }, {
-        show: false,
         name: "2018-02",
         children: [{
           name: "文章1",
-          url: "#"
+          url: "//baidu.com"
         }, {
           name: "文章2",
-          url: "#"
+          url: "//baidu.com"
         }]
       }]
     }, {
-      show: false,
       name: 2017,
       children: [{
         show: false,
@@ -118,7 +108,6 @@ new Vue({
           url: "#"
         }]
       }, {
-        show: false,
         name: "2017-02",
         children: [{
           name: "文章1",
@@ -129,55 +118,53 @@ new Vue({
         }]
       }]
     }];
+    var _this = this;
+    this.dataList.forEach(function (item) {
+      _this.$set(item, 'show', false);
+      item.children.forEach(function (item2) {
+        _this.$set(item2, 'show', false);
+      });
+    });
   },
   methods: {
-    toggleShare: function toggleShare(item) {
-      var ev = ev || event;
-      var idx = ev.target.getAttribute('index');
-      var len = ev.target.parentNode.parentNode.children.length;
-      item.show = false;
-      for (var i = 0; i < len; i++) {
-        var child = ev.target.parentNode.parentNode.children[i];
-        var tagUl = child.getElementsByTagName('ul')[0];
-        var tagP = child.getElementsByTagName('p')[0];
-        if (i == idx) {
-          if (tagP.className == '') {
-            tagP.className = 'active';
-            tagUl.style.display = 'block';
-          } else {
-            item.show = true;
-          }
+    resetDelStatus: function resetDelStatus() {
+      var _this = this;
+      this.dataList.forEach(function (item) {
+        item.children.forEach(function (item2) {
+          item2.children.forEach(function (item3) {
+            if (typeof item3.style != 'undefined' && Number(item3.style.match(/\d+/)) != 0) {
+              _this.$set(item3, 'style', 'transform: translateX(0px)');
+            }
+          });
+        });
+      });
+    },
+    toggle: function toggle(current) {
+      this.dataList.forEach(function (item) {
+        if (item == current) {
+          item.show = !item.show;
         } else {
-          tagP.className = '';
-          tagUl.style.display = 'none';
+          item.show = false;
         }
-      }
-      //重置del
-      var tagPList = ev.target.parentNode.parentNode.getElementsByTagName('p');
-      for (var j = 0; j < tagPList.length; j++) {
-        var t = tagPList[j].style.transform;
-        if (typeof t != 'undefined' && Number(t.match(/\d+/)) > 0) {
-          tagPList[j].style.transform = 'translateX(0px)';
-        }
-      }
-
-      item.show = !item.show;
+        item.children.forEach(function (item2) {
+          if (item2.show) {
+            item2.show = false;
+          }
+        });
+      });
+      this.resetDelStatus();
     },
-    toggle: function toggle(item) {
-      this.toggleShare(item);
-
-      //重置2级ul
-      var ev = ev || event;
-      var tagP = ev.target.nextSibling.nextSibling.getElementsByTagName('p');
-      for (var i = 0; i < tagP.length; i++) {
-        if (tagP[i].className == 'active') {
-          tagP[i].className = '';
-          tagP[i].nextSibling.nextSibling.style.display = 'none';
-        }
-      }
-    },
-    toggle2: function toggle2(item) {
-      this.toggleShare(item);
+    toggle2: function toggle2(current) {
+      this.dataList.forEach(function (item) {
+        item.children.forEach(function (item2) {
+          if (item2 == current) {
+            item2.show = !item2.show;
+          } else {
+            item2.show = false;
+          }
+        });
+      });
+      this.resetDelStatus();
     }
   }
 });
