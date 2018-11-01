c5f525435e0d655b4f044bdd35d81cb83ea83094
ZhuHongDa <zhuhongda@mallcoo.cn>
2018-11-01 16:38:59 +0800

    add

--- memo/a/assets/js/resize.js
+++
@@ -1,9 +0,0 @@
-/* 长宽占位 rem算法, 根据root的rem来计算各元素相对rem, 默认html 750/10 = 75px */
-updataHtml();
-function updataHtml() {
-    var w = document.documentElement.offsetWidth;
-    document.documentElement.style.fontSize = w / 10 + 'px';
-}
-window.onresize = function () {
-    updataHtml();
-}
