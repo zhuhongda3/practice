d29a4f3d92758127a472c4083dc516903260342d
zhuhongda <zhuhongda@mallcoo.cn>
2019-12-23 14:12:25 +0800

    add

--- react-project/my-app/src/index.js
+++ react-project/my-app/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom'
 
 // import TodoList from './components/TodoList/TodoList';
 // import RouterExample from './components/RouterDemo/RouterExample'
-// import ClockDemo from './components/ClockDemo/ClockDemo'
+import ClockDemo from './components/ClockDemo/ClockDemo'
 // import LoginControl from './components/LoginControl/LoginControl'
-import NameForm from './components/NameForm/NameForm'
+// import NameForm from './components/NameForm/NameForm'
 
-ReactDOM.render(<NameForm />, document.getElementById('root'))
+ReactDOM.render(<ClockDemo />, document.getElementById('root'))
