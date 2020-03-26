d2a8b1c06872f68ff95c7ecadc1911e05d0c4c81
zhuhongda <zhuhongda@mallcoo.cn>
2020-03-26 23:01:31 +0800

    add

--- memo/js/global.js
+++ memo/js/global.js
@@ -1,64 +1,58 @@
-$(document).ready(function () {
-    $(".link-item").eq(0).show().siblings().hide();
-    $(".link,.menu-con li").click(function () {
-        var i = $(this).index();
-        // $("html, body").animate({
-        //     scrollTop: $(".link-item").eq(i).offset().top
-        // }, {
-        //     duration: 500,
-        //     easing: "swing"
-        // });
-        $(this).addClass('active').siblings().removeClass('active')
-        $(".link-item").eq(i).show().siblings().hide();
-        return false;
-    });
+$(document).ready(function() {
+  $(".link-item")
+    .eq(0)
+    .show()
+    .siblings()
+    .hide();
+  $(".link,.menu-con li").click(function() {
+    var i = $(this).index();
+    // $("html, body").animate({
+    //     scrollTop: $(".link-item").eq(i).offset().top
+    // }, {
+    //     duration: 500,
+    //     easing: "swing"
+    // });
+    $(this)
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
+    $(".link-item")
+      .eq(i)
+      .show()
+      .siblings()
+      .hide();
+    return false;
+  });
 
-    $(".go-top").click(function () {
-        $("html, body").animate({
-            'scrollTop': '0'
-        }, 300);
-    });
+  $(".go-top").click(function() {
+    $(".link-list").animate(
+      {
+        scrollTop: "0"
+      },
+      300
+    );
+  });
 
-    $(window).scroll(function () {
-        $('.go-top').addClass('off-inner');
-        var $this = $('.title-bar-right p');
-        $this.hide();
-        var scrollTop = $(this).scrollTop();
-        if (scrollTop > 0) {
-            $this.hide();
-        } else {
-            $this.show();
-        }
-    });
-
-    function checkScroll() {
-        var m1 = 0,
-            m2 = 0,
-            timer = null,
-            that = this;
-        document.onscroll = function () {
-            clearTimeout(that.timer) // 每次滚动前 清除一次
-            that.timer = setTimeout(function () {
-                that.m2 = $(window).scrollTop();
-                if (that.m2 == that.m1) {
-                    $('.go-top').removeClass('off-inner');
-                }
-            }, 700);
-            that.m1 = $(window).scrollTop();
-        }
+  $(".link-list").scroll(function() {
+    var $this = $('.go-top');
+    var scrollTop = $(".link-list").scrollTop();
+    if (scrollTop > 200) {
+      $this.show();
+    } else {
+      $this.hide();
     }
-    checkScroll();
+  });
 
-    // var grid = new Minigrid({
-    //     container: '.link-list',
-    //     item: '.link-item',
-    //     gutter: 12
-    // });
-    // grid.mount();
-    // window.addEventListener('resize', grid.mount());
-    // $(window).resize(function () {
-    //     grid.mount();
-    // });
+  // var grid = new Minigrid({
+  //     container: '.link-list',
+  //     item: '.link-item',
+  //     gutter: 12
+  // });
+  // grid.mount();
+  // window.addEventListener('resize', grid.mount());
+  // $(window).resize(function () {
+  //     grid.mount();
+  // });
 
-    $(document).foundation();
+  $(document).foundation();
 });
